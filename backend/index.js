964fef7166407dac3a1c9edc49a42d4266472284
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-05-13 20:03:28 -0300

    Log middleware.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { controllers } from './controllers/controllers.js';
 import { errorHandlerMiddleware } from './middlewares/error_handler_middleware.js';
+import { logMiddleware } from './middlewares/log_middleware.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(logMiddleware);
 
 controllers(app);
 
