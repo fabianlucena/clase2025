ef75c7a46796d97d69a717dba506786c5467ba72
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2025-08-12 19:05:29 -0300

    Add Snackbar component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,21 @@ import './App.css';
 import Head from './components/Head.jsx';
 import Body from './components/Body.jsx';
 import { ModalProvider } from './components/Modal.jsx';
+import { SnackbarProvider } from './components/Snackbar.jsx';
 
 export default function App() {
   const [menuShowed, setMenuShowed] = useState(false);
 
   return (
-    <ModalProvider>
-      <Head
-        setMenuShowed={setMenuShowed}
-      />
-      <Body
-        menuShowed={menuShowed}
-      />
-    </ModalProvider>
+    <SnackbarProvider>
+      <ModalProvider>
+        <Head
+          setMenuShowed={setMenuShowed}
+        />
+        <Body
+          menuShowed={menuShowed}
+        />
+      </ModalProvider>
+    </SnackbarProvider>
   )
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react';
 import Form from './Form.jsx';
 import TextField from './TextField.jsx';
 import { useModal } from './Modal.jsx';
+import { useSnackbar } from './Snackbar.jsx';
 
 export default function Login() {
   const [username, setUsername] = useState('1');
   const [password, setPassword] = useState('2');
   const { open: openModal } = useModal();
+  const snackbar = useSnackbar();
 
   function submit() {
-    openModal('Ingresando');
+    //openModal('Ingresando');
+    snackbar.enqueue('Ingresando', { variant: 'info', timeout: 5000 });
+    snackbar.enqueue('Ingreso OK', { variant: 'success', timeout: 6000 });
+    snackbar.enqueue('Error de ingreso', { variant: 'error', timeout: 6500 });
     /*setTimeout(
       () => modal.close(),
       2500
